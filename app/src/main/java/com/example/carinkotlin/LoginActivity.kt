b20b5f5ffa123a10a24ab5d116e2c262ddc2216b
hamadfauzi <fauzihamad37@gmail.com>
2019-06-08 23:44:03 +0700

    membuat kode untuk melakukan login

--- app/src/main/java/com/example/carinkotlin/LoginActivity.kt
+++ app/src/main/java/com/example/carinkotlin/LoginActivity.kt
@@ -5,6 +5,15 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import android.R.attr.password
+import com.google.firebase.auth.FirebaseAuth
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -12,7 +21,7 @@ class LoginActivity : AppCompatActivity() {
     lateinit var  etPassword : EditText
     lateinit var  btnToRegister : Button
     lateinit var  btnLogin : Button
-
+    lateinit var  mAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +35,44 @@ class LoginActivity : AppCompatActivity() {
         etPassword = findViewById(R.id.inputPassword) as EditText
         btnToRegister = findViewById(R.id.btnToRegisterActivity) as Button
         btnLogin = findViewById(R.id.btnLogin) as Button
-
+        mAuth = FirebaseAuth.getInstance()
         btnToRegister.setOnClickListener{
             val registerIntent = Intent(this,RegisterActivity::class.java)
             startActivity(registerIntent)
+        }
+        btnLogin.setOnClickListener {
+            signInUser()
+        }
+
+    }
+
+    private fun signInUser()
+    {
+        val email : String = etEmail.text.toString()
+        val password : String = etPassword.text.toString()
+
+        if(email.isNullOrEmpty())
+        {
+            Toast.makeText(applicationContext,"Enter your email ",Toast.LENGTH_SHORT).show()
+        }
+        else if (password.isNullOrEmpty())
+        {
+            Toast.makeText(applicationContext,"Enter Your Password",Toast.LENGTH_SHORT).show()
+        }
+        else
+        {
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, OnCompleteListener<AuthResult> { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(applicationContext,"Login Success",Toast.LENGTH_SHORT).show()
+                        val homeIntent = Intent(this,MainActivity::class.java)
+                        startActivity(homeIntent)
+                        finish()
+                    } else {
+                        Toast.makeText(applicationContext,"Login Failed",Toast.LENGTH_SHORT).show()
+                    }
+
+                })
         }
     }
 }
